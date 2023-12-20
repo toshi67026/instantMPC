79c3a67038e6e711c608d06e6a2409297b7ab8f2
“Toshiyuki <toshiyuki67026@gmail.com>
2023-12-20 14:28:12 +0900

    fix: label, readme

--- src/main.m
+++ src/main.m
@@ -14,7 +14,7 @@ hold on
 plot(simOut.tout, simOut.x.Data(:, 1), 'LineWidth', 3)
 yline(r(1), '-.', 'LineWidth', 3)
 legend({'$x$', '$i_r$'}, 'Interpreter', 'latex')
-xlabel('Time [s]')
+xlabel('Time [s]', 'Interpreter', 'latex')
 ylabel('$i$ [A]', 'Interpreter', 'latex')
 grid on
 box on
@@ -25,7 +25,7 @@ hold on
 plot(simOut.tout, simOut.x.Data(:, 2), 'LineWidth', 3)
 yline(r(2), '-.', 'LineWidth', 3)
 legend({'$\omega$', '$\omega_r$'}, 'Interpreter', 'latex')
-xlabel('Time [s]')
+xlabel('Time [s]', 'Interpreter', 'latex')
 ylabel('$\omega$ [rad/s]', 'Interpreter', 'latex')
 grid on
 box on
@@ -35,7 +35,7 @@ figure
 hold on
 plot(simOut.tout, simOut.opt_value.Data, 'LineWidth', 3)
 xlabel('Time [s]', 'Interpreter', 'latex')
-ylabel('optimal value')
+ylabel('optimal value', 'Interpreter', 'latex')
 grid on
 box on
 set(gca, 'FontSize', 24)
@@ -43,7 +43,7 @@ set(gca, 'FontSize', 24)
 figure
 hold on
 plot(simOut.tout, simOut.u.Data, 'LineWidth', 3)
-xlabel('Time [s]')
+xlabel('Time [s]', 'Interpreter', 'latex')
 ylabel('$u$', 'Interpreter', 'latex')
 grid on
 box on
